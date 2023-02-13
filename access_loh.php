d357b0fb102364c4ee0e2ac158757650c0eedcc2
CodeTiALF <117958536+CodeTiALF@users.noreply.github.com>
2023-02-12 21:39:13 -0300

    Prefixo log_ nas variaveis
    Acrescentei o prefixo "log_" nas variáveis para evitar que em futuros módulos aconteça de haver mesma variável declarada

--- access_loh.php
+++ access_loh.php
@@ -3,126 +3,126 @@
 // Colocar no index.php include "access_log/";
 // Conexão com o banco de dados MySQL
 // Obtém o endereço IP do visitante
-$ip = $_SERVER['REMOTE_ADDR'];
+$log_ip = $_SERVER['REMOTE_ADDR'];
 // Obtém a URL visitada
-$url = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
-$agente = $_SERVER['HTTP_USER_AGENT'];
-$cookie = $_SERVER['HTTP_COOKIE'];
+$log_url = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
+$log_agente = $_SERVER['HTTP_USER_AGENT'];
+$log_cookie = $_SERVER['HTTP_COOKIE'];
 // Define o nome da tabela e automaticamente do arquivo de log
-$path = "access_log";
-$tabela = "access_log_pagina_inicial";
-$file = $path."/".$tabela.".txt";
+if (!isset($log_path)) { $log_path = "access_log"; }
+if (!isset($log_tabela)) { $log_tabela = "access_log_pagina_inicial"; }
+if (!isset($log_file)) { $log_file = $log_path."/".$log_tabela.".txt"; }
 
 // Conexão com o banco de dados MySQL
-$conn = mysqli_connect("localhost", "xxxxx", "xxxxx", "xxxxx");
+$log_conn = mysqli_connect("localhost", "xxxxx", "xxxxxx", "xxxxx");
 // Verifica se a conexão foi estabelecida com sucesso
-if (!$conn) {
+if (!$log_conn) {
     die("Conexão com o banco de dados falhou: " . mysqli_connect_error());
 }
 // Verifica se a tabela `access_log` existe
-$table_check = "SHOW TABLES LIKE '".$tabela."'";
-$result = mysqli_query($conn, $table_check);
-if (mysqli_num_rows($result) == 0) {
+$log_table_check = "SHOW TABLES LIKE '".$log_tabela."'";
+$log_result = mysqli_query($log_conn, $log_table_check);
+if (mysqli_num_rows($log_result) == 0) {
     // Cria a tabela `access_log`
-    $table_create = "CREATE TABLE {$tabela} (
+    $log_table_create = "CREATE TABLE {$log_tabela} (
         id INT(11) AUTO_INCREMENT PRIMARY KEY,
         datetime DATETIME NOT NULL,
         ip VARCHAR(15) NOT NULL,
         url VARCHAR(255) NOT NULL
     )";
-    mysqli_query($conn, $table_create);
+    mysqli_query($log_conn, $log_table_create);
 }
 
 // Prepara a consulta SQL para inserir os dados na tabela `access_log`
-$sql = "INSERT INTO {$tabela} (datetime, ip, url)
-VALUES (NOW(), '$ip', '$url')";
+$log_sql = "INSERT INTO {$log_tabela} (datetime, ip, url)
+VALUES (NOW(), '$log_ip', '$log_url')";
 
 // Executa a consulta SQL
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($log_conn, $log_sql)) {
     // echo "Dados inseridos com sucesso na tabela `access_log`";
 } else {
-    // echo "Erro ao inserir dados na tabela `access_log`: " . mysqli_error($conn);
+    // echo "Erro ao inserir dados na tabela `access_log`: " . mysqli_error($log_conn);
 }
 
 // Fecha a conexão com o banco de dados
-mysqli_close($conn);
+mysqli_close($log_conn);
 
 
 
 
 // Verifica se o arquivo existe
-if (!file_exists($file)) {
+if (!file_exists($log_file)) {
     // Abre o arquivo para escrita
-    $handle = fopen($file, "w");
+    $log_handle = fopen($log_file, "w");
 
     // Verifica se o arquivo foi aberto com sucesso
-    if ($handle) {
+    if ($log_handle) {
         // Escreve a primeira linha no arquivo
-        fwrite($handle, "--- Log de Ip visitantes ---------------------------------------\n");
+        fwrite($log_handle, "--- Log de Ip visitantes ---------------------------------------\n");
 
         // Escreve a segunda linha no arquivo
-        fwrite($handle, "----------------------------------------------------------------\n");
+        fwrite($log_handle, "----------------------------------------------------------------\n");
 
         // Deixa a terceira linha vazia
-        fwrite($handle, "\n");
+        fwrite($log_handle, "\n");
 
         // Fecha o arquivo
-        fclose($handle);
+        fclose($log_handle);
     }
 }
 
 // Inicializa a variável de contagem de visitas
-$count = 0;
+$log_count = 0;
 
 // Abre o arquivo para leitura
-$handle = fopen($file, "r");
+$log_handle = fopen($log_file, "r");
 
 // Verifica se o arquivo foi aberto com sucesso
-if ($handle) {
+if ($log_handle) {
     // Lê todo o conteúdo do arquivo em uma string
-    $fileContent = fread($handle, filesize($file));
+    $log_fileContent = fread($log_handle, filesize($log_file));
 
     // Fecha o arquivo
-    fclose($handle);
+    fclose($log_handle);
 
     // Converte o conteúdo do arquivo em um array, dividido por linhas
-    $fileLines = explode("\n", $fileContent);
+    $log_fileLines = explode("\n", $log_fileContent);
 
     // Percorre cada linha do arquivo
-    foreach ($fileLines as $line) {
+    foreach ($log_fileLines as $log_line) {
         // Verifica se o IP do visitante já está registrado no arquivo
-        if (strpos($line, $ip) !== false) {
+        if (strpos($log_line, $log_ip) !== false) {
             // Incrementa a contagem de visitas
-            $count++;
+            $log_count++;
         }
     }
 }
 
 // Incrementa a contagem de visitas
-$count++;
+$log_count++;
 
 // Monta a string com a data e o IP
-$newLine = date("Y-m-d H:i:s") . " - " . $ip . " - " . $count . " visita(s)";
+$log_newLine = "| ".date("Y-m-d H:i:s") . " | " . $log_ip . " | " . $log_count . " visita(s) | ".$log_url;
 
 // Insere a nova linha na posição 3 (índice 2)
-array_splice($fileLines, 2, 0, $newLine);
+array_splice($log_fileLines, 2, 0, $log_newLine);
 
 // Junta as linhas do array novamente em uma string
-$fileContent = implode("\n", $fileLines);
+$log_fileContent = implode("\n", $log_fileLines);
 
 // Abre o arquivo para escrita
-$handle = fopen($file, "w");
+$log_handle = fopen($log_file, "w");
 
 // Verifica se o arquivo foi aberto com sucesso
-if ($handle) {
+if ($log_handle) {
     // Escreve a nova string no arquivo
-    fwrite($handle, $fileContent);
+    fwrite($log_handle, $log_fileContent);
 
     // Fecha o arquivo
-    fclose($handle);
+    fclose($log_handle);
 }
 
 // Exibe a mensagem de saudação
-// echo "Este é sua visita número " . $count . " hoje!";
+// echo "Este é sua visita número " . $log_count . " hoje!";
 
 ?>
