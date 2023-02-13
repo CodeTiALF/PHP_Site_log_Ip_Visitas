56e377ac83e1f7422d9a52d8e98c6aea57cba769
CodeTiALF <117958536+CodeTiALF@users.noreply.github.com>
2023-02-12 21:54:17 -0300

    Index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+  // Define caminho do arquivo access_log.ph na variavel $log_path
+	$log_path = "access_log";
+  // Define o nome da tabela e que será usado como nome do arquio log, na variavel $log_tabela 
+	$log_tabela = "access_log_smallweb_raiz";
+	// Inclui o codigo
+	include $log_path."/access_log.php"; 
+?>
